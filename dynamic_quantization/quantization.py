7aea18935745ac99bca21c50909b6ab8a7df858b
Lakshmi TS <lachuthattarakkad@gmail.com>
2025-04-10 16:22:37 +0530

    added onnx conversion and quantisation files

--- dynamic_quantization/quantization.py
+++ dynamic_quantization/quantization.py
@@ -1,7 +1,7 @@
 from onnxruntime.quantization import quantize_dynamic, QuantType
 
 # Apply dynamic quantization
-quantized_model_path = "D:/prodigal-4/models/yolo12n.quantized.onnx"
-quantize_dynamic("D:/prodigal-4/models/yolo12n.onnx", quantized_model_path, weight_type=QuantType.QUInt8)
+quantized_model_path = "C:/Users/user/Desktop/yolov12_quant_api/models/yolo12n.quantized.onnx"
+quantize_dynamic("C:/Users/user/Desktop/yolov12_quant_api/models/distilled_pruned_model/weights/best.onnx", quantized_model_path, weight_type=QuantType.QUInt8)
 
 print(f"Quantized model saved at {quantized_model_path}")
