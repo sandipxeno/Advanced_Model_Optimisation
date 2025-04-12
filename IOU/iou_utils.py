a81cbb2890dbcaeaf5d679ae09d5bc18a5885a69
karthikeya <139058953+karthikeyapranav@users.noreply.github.com>
2025-04-12 17:54:05 +0530

    added iou folder

---
+++ IOU/iou_utils.py
@@ -0,0 +1,18 @@
+def calculate_iou(box1, box2):
+    """
+    Calculate Intersection over Union (IoU) between two boxes.
+    box1 and box2: format [x1, y1, x2, y2]
+    """
+    xA = max(box1[0], box2[0])
+    yA = max(box1[1], box2[1])
+    xB = min(box1[2], box2[2])
+    yB = min(box1[3], box2[3])
+
+    interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+    box1Area = (box1[2] - box1[0] + 1) * (box1[3] - box1[1] + 1)
+    box2Area = (box2[2] - box2[0] + 1) * (box2[3] - box2[1] + 1)
+
+    unionArea = box1Area + box2Area - interArea
+    iou = interArea / unionArea if unionArea != 0 else 0
+
+    return iou
